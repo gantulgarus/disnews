4a2039af03bcff8a5b18c18cb25640567dafbba6
gantulgarus <gantulgarus@gmail.com>
2025-07-29 15:10:08 +0800

    update fron gantu

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\PowerPlantController;
 use App\Http\Controllers\DailyBalanceJournalController;
 use App\Http\Controllers\PowerPlantDailyReportController;
 use App\Http\Controllers\UserController;
-use App\Http\Controllers\TnewsController; 
+use App\Http\Controllers\TnewsController;
 
 
 
@@ -23,19 +23,17 @@ Route::get('/', [DashboardController::class, 'index'])->middleware(['auth', 'ver
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-   // Route::post('/users', [UserController::class, 'store'])->name('users.store');
+    // Route::post('/users', [UserController::class, 'store'])->name('users.store');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     Route::resource('power-plant-daily-reports', PowerPlantDailyReportController::class);
     Route::resource('power-plants', PowerPlantController::class);
+    Route::resource('daily-balance-journals', DailyBalanceJournalController::class);
+    Route::get('daily-balance-journals/report', [DailyBalanceJournalController::class, 'dailyMatrixReport'])->name('daily-balance-journals.report');
 
     Route::resource('users', UserController::class);
     Route::resource('tnews', TnewsController::class);
-    
-
-
 });
 
 require __DIR__ . '/auth.php';
-
