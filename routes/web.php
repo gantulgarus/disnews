500aba4c1031c8c22772877f591266877578edc8
gantulgarus <gantulgarus@gmail.com>
2025-09-24 14:27:48 +0800

    web update

--- routes/web.php
+++ routes/web.php
@@ -39,25 +39,4 @@ Route::middleware('auth')->group(function () {
     Route::resource('power-distribution-works', PowerDistributionWorkController::class);
 });
 
-Route::get('/test-db', function () {
-    try {
-        DB::connection('second_db')->getPdo();
-        return "Second DB connection works!";
-    } catch (\Exception $e) {
-        return "Connection failed: " . $e->getMessage();
-    }
-});
-
-Route::get('/debug-db', function () {
-    $config = config('database.connections.second_db');
-    return response()->json([
-        'host' => $config['host'],
-        'port' => $config['port'],
-        'database' => $config['database'],
-        'username' => $config['username'],
-        // 'socket' => $config['unix_socket'],
-        'full_config' => $config
-    ]);
-});
-
 require __DIR__ . '/auth.php';
