20eb40004adb3b22769772fab836a929b8d3afcc
gantulgarus <gantulgarus@gmail.com>
2026-01-02 16:56:48 +0800

    fix

--- app/Http/Controllers/BufVIntController.php
+++ app/Http/Controllers/BufVIntController.php
@@ -25,10 +25,11 @@ class BufVIntController extends Controller
             ->whereIn('N_GR_TY', [1, 2])
             ->whereIn('N_FID', [257, 258, 110])
             ->whereRaw('DATE(DD_MM_YYYY) = CURDATE()')
-            ->groupBy('N_INTER_RAS', 'N_FID')
+            ->groupBy('N_INTER_RAS', 'N_FID', DB::raw('DATE(DD_MM_YYYY)'), DB::raw('CONCAT(LPAD(FLOOR((N_INTER_RAS-1)/2),2,"0"),":",CASE WHEN MOD(N_INTER_RAS,2)=1 THEN "00" ELSE "30" END,"-",LPAD(FLOOR((N_INTER_RAS-1)/2),2,"0"),":",CASE WHEN MOD(N_INTER_RAS,2)=1 THEN "30" ELSE "00" END)'))
             ->orderBy('N_INTER_RAS')
             ->orderBy('N_FID')
             ->get();
+
 
         // Pivot хэлбэрт хувиргах: цаг -> фидер -> import/export
         $pivot = [];
