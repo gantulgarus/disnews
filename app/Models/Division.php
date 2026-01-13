4b53a897405d120d370e63a5ee2c1cb1ff783011
gantulgarus <gantulgarus@gmail.com>
2026-01-13 12:04:23 +0800

    update

--- app/Models/Division.php
+++ app/Models/Division.php
@@ -7,14 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Division extends Model
 {
     protected $fillable = ['Div_name', 'Div_code'];
-    protected $primaryKey = 'Div_code';
-    public $incrementing = false;
+    // protected $primaryKey = 'Div_code';
+    // public $incrementing = false;
 
 
     public function users()
     {
         return $this->hasMany(User::class, 'div_code', 'div_code');
     }
-
 }
-
